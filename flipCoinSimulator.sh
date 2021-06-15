0227e02a029e636f2bd351fdbd702855b47e4495
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 20:02:35 +0530

    Refactoring code to continue till either of them won 21 times and print who won by what

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,18 +1,31 @@
-#!/bin/bash -x
+#!/bin/bash 
 read -p "How many times to loop" n
 no_of_heads=0
 no_of_tails=0
-for((i=0;i<n;i++))
+while [ $no_of_heads -lt 21 -a $no_of_tails -lt 21 ]
 do
-random=$((RANDOM%2))
-if [ $random -eq 1 ]
-then
-echo "Heads"
-no_of_heads=$(($no_of_heads+1))
-echo "Number of heads = $no_of_heads"
-else
-echo "Tail"
-no_of_tails=$(($no_of_tails+1))
-echo "Number of tails $no_of_tails"
-fi
+	for((i=0;i<n;i++))
+	do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+	echo "Heads"
+	no_of_heads=$(($no_of_heads+1))
+	echo "Number of heads = $no_of_heads"
+	else
+	echo "Tail"
+	no_of_tails=$(($no_of_tails+1))
+	echo "Number of tails $no_of_tails"
+	fi
+	done
 done
+		if [ $no_of_heads -gt $no_of_tails ]
+		then
+		echo "Heads won by $no_of_heads times"
+		elif [ $no_of_heads -lt $no_of_tails ]
+		then
+		echo "Tails won by $no_of_tails times"
+		else
+		echo "Its a Tie"
+		fi
+
