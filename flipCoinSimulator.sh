28cfa71da25bb9531cdd41cb21291c955b21efc6
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 20:32:55 +0530

    Refactored the code to continue if tie till difference of minimum 2 points is achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,8 @@
 #!/bin/bash 
-read -p "How many times to loop" n
 no_of_heads=0
 no_of_tails=0
 while [ $no_of_heads -lt 21 -a $no_of_tails -lt 21 ]
 do
-	for((i=0;i<n;i++))
-	do
 	random=$((RANDOM%2))
 	if [ $random -eq 1 ]
 	then
@@ -18,14 +15,20 @@ do
 	echo "Number of tails $no_of_tails"
 	fi
 	done
-done
+
 		if [ $no_of_heads -gt $no_of_tails ]
 		then
 		echo "Heads won by $no_of_heads times"
+		exit
 		elif [ $no_of_heads -lt $no_of_tails ]
 		then
 		echo "Tails won by $no_of_tails times"
-		else
+		exit
+		elif [ $no_of_heads -eq $no_of_tails ]
+		then
 		echo "Its a Tie"
+		while [ $no_of_heads - $no_of_tails -eq 2 -a $no_of_tails - $no_of_heads -eq 2 ]
+		do
+		random=$((RANDOM%2))
+		done
 		fi
-
