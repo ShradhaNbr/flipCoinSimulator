3a9f74bbab887e96f27bdd8a5ec7b4ce434d9123
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 19:39:29 +0530

    Refactoring code to loop through multiple times and printing number of times head and tail has won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,18 @@
 #!/bin/bash -x
+read -p "How many times to loop" n
+no_of_heads=0
+no_of_tails=0
+for((i=0;i<n;i++))
+do
 random=$((RANDOM%2))
 if [ $random -eq 1 ]
 then
 echo "Heads"
+no_of_heads=$(($no_of_heads+1))
+echo "Number of heads = $no_of_heads"
 else
 echo "Tail"
+no_of_tails=$(($no_of_tails+1))
+echo "Number of tails $no_of_tails"
 fi
+done
